b57972eb60f68ebf6b06fae12b2b1ab506fcde67
lamh85 <lamh85@gmail.com>
2022-05-12 22:22:12 -0700

    Create gitignore and index file.

---
+++ src/index.sh
@@ -0,0 +1,5 @@
+alias re_source='source ~/.zshrc'
+alias get_weather='curl wttr.in/Coquitlam'
+alias get_ip='ipconfig getifaddr en0'
+alias cd_projects='cd /Users/Hon-Lam/Projects'
+alias edit_source="code ~/.zshrc"
